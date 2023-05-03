fadf7b11c9237604d729180be0b38a9fb8b81cfc
night-glider <nightgliderdev@gmail.com>
2023-05-03 15:22:41 +0300

    fixed routing in lesson controller

--- src/Controller/LessonController.php
+++ src/Controller/LessonController.php
@@ -37,7 +37,11 @@ class LessonController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $lessonRepository->add($lesson, true);
 
-            return $this->redirectToRoute('app_lesson_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute(
+                'app_course_show',
+                ['id' => $lesson->getCourse()->getId()],
+                Response::HTTP_SEE_OTHER
+            );
         }
 
         return $this->renderForm('lesson/edit.html.twig', [
@@ -55,6 +59,10 @@ class LessonController extends AbstractController
             $lessonRepository->remove($lesson, true);
         }
 
-        return $this->redirectToRoute('app_lesson_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute(
+            'app_course_show',
+            ['id' => $lesson->getCourse()->getId()],
+            Response::HTTP_SEE_OTHER
+        );
     }
 }
